aa1e7503e514f2b104f4d050a4a43ec01de34d20
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-08-01 08:52:36 -0400

    wrote a custom method to display all of the student names enrolled in a teacher's courses.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,12 @@ class Student < ApplicationRecord
   has_many :enrollments
   has_many :courses, through: :enrollments
   has_many :teachers, through: :courses
+
+  def student_name(student_id)
+    User.all.each do |user|
+      if user.role_id == student_id && user.role_type == "Student"
+        return user.name
+      end
+    end
+  end
 end
