3c25b13a667ad311e80ffb866a07b6586ad70e24
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-08-01 09:06:34 -0400

    added another custom method to show the teachers name that is teaching each course

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,12 @@ class Course < ApplicationRecord
   has_many :enrollments
   has_many :students, through: :enrollments
   belongs_to :teacher
+
+  def course_teacher(course)
+    User.all.each do |user|
+      if course.teacher.id == user.role_id && user.role_type == "Teacher"
+        return user.name
+      end
+    end
+  end
 end
