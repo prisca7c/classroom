ceb63937a716882710bcacd0628df2170ded7a65
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-08-01 08:26:54 -0400

    added a link from the course index back to the profile

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,7 +6,7 @@ class SessionController < ApplicationController
 
    def create
     user = User.find_by_email(params[:user][:email])
-    if user.authenticate(params[:user][:password])
+    if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
       redirect_to profiles_path(user)
     else
