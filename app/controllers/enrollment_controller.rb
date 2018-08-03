768e79f7e0008e5b7dcff69331f56686396ee68c
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-08-03 11:07:36 -0400

    Organized courses on the student show page by semester

--- app/controllers/enrollment_controller.rb
+++ app/controllers/enrollment_controller.rb
@@ -8,7 +8,7 @@ class EnrollmentController < ApplicationController
     # binding.pry
     course = Course.find(params[:course][:name])
     student = User.find(session[:user_id])
-    student.role.enrollments.build(course_id: course.id, student_id: student.id, semester: params[:semester]).save
+    student.role.enrollments.build(course_id: course.id, student_id: student.id).save
     redirect_to profiles_path(student)
   end
 end
