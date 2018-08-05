630d2d8b6f6072259ba7bf415d930c8a7a4c0568
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-08-05 16:15:30 -0400

    added a grades controller and nested courses under profiles

---
+++ app/controllers/grades_controller.rb
@@ -0,0 +1,12 @@
+class GradesController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
-  resource :profiles
-  resources :courses, only: [:new, :create, :index]
+  resource :profiles, only: [:show] do
+    resources :courses, only: [:new, :create, :index]
+  end
+
   resources :teacher_login, only: [:new, :create]
   resources :student_login, only: [:new, :create]
   resources :session, only: [:new, :create, :destroy]
   resources :enrollment, only: [:new, :create]
+  resources :grades, only: [:new,:create,:index, :show]
   root 'static_pages#home'
 end
