6f7c512a8809adb1e3f8a7c5c81e28b4f26a2a36
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-09-07 19:14:36 -0400

    added a seperated appender method for Winter Courses and Fall Courses to prevent the data from appending twice.

--- app/assets/javascripts/gradeshowpage.js
+++ app/assets/javascripts/gradeshowpage.js
@@ -1,8 +1,9 @@
 class Appender {
-  courseIndexPage(data) {
+  fallCourseIndexPage(data) {
     let courseName = data["name"]
     let courseId = data["id"]
     let courseTeacher = data["teacher"]["user"]["name"]
+    let semester = data["semester"]
     let studentArray = data["students"]
     function test(studentArray) {
       let emptyArray = []
@@ -11,6 +12,22 @@ class Appender {
       }
       return emptyArray.join("")
     }
-    $(`div.${courseId}`).append('<table class="table table-striped"><thead><tr><th scope="col">Role</th><th scope="col">Name</th></tr></thead><tbody><tr><th scope="row">Instructor</th><td class="instructor">' + courseTeacher + '</td></tr>' + test(studentArray) + '</tbody></table>')
+    $(`div.${courseId}`).append('Semester: ' + semester + '<table class="table table-striped"><thead><tr><th scope="col">Role</th><th scope="col">Name</th></tr></thead><tbody><tr><th scope="row">Instructor</th><td class="instructor">' + courseTeacher + '</td></tr>' + test(studentArray) + '</tbody></table>')
+  }
+
+  winterCourseIndexPage(data) {
+    let courseName = data["name"]
+    let courseId = data["id"]
+    let courseTeacher = data["teacher"]["user"]["name"]
+    let semester = data["semester"]
+    let studentArray = data["students"]
+    function test(studentArray) {
+      let emptyArray = []
+      for (var i = 0; i < studentArray.length; i++) {
+        emptyArray.push('<tr><th scope="row">Student</th><td class="studentName">' + studentArray[i]["user"]["name"] + '</td></tr>')
+      }
+      return emptyArray.join("")
+    }
+    $(`div.${courseId}`).append('Semester: ' + semester + '<table class="table table-striped"><thead><tr><th scope="col">Role</th><th scope="col">Name</th></tr></thead><tbody><tr><th scope="row">Instructor</th><td class="instructor">' + courseTeacher + '</td></tr>' + test(studentArray) + '</tbody></table>')
   }
 }
