4b689682f10ce8b1446e3683a6da8e0a0e6d3a9a
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-09-10 08:26:46 -0400

    created new instance methods on the appender class and cleaned up the student show view with them

--- app/assets/javascripts/gradeshowpage.js
+++ app/assets/javascripts/gradeshowpage.js
@@ -30,4 +30,35 @@ class Appender {
     }
     $(`div.${courseId}`).append('Semester: ' + semester + '<table class="table table-striped"><thead><tr><th scope="col">Role</th><th scope="col">Name</th></tr></thead><tbody><tr><th scope="row">Instructor</th><td class="instructor">' + courseTeacher + '</td></tr>' + test(studentArray) + '</tbody></table>')
   }
+
+  studentShowPageFall(data) {
+    let emptyArray = []
+    function dataCollector(data) {
+    for (var i = 0; i < data.length; i++) {
+      let courseName = data[i]["course"]["name"]
+      let grade = data[i]["grade"]["letter"]
+      let enrolledOnRaw = data[i]["created_at"].split("T")[0]
+      // var enrolledOn = enrolledOnRaw.split("T")[1]
+      emptyArray.push('<tr><th scope="row">' + courseName + '</th><td>' + grade + '</td><td>' + enrolledOnRaw + '</td></tr>')
+    }
+    return emptyArray.join("")
+    }
+    $('div.fallinfo').append('<table class="table table-striped"<thead><tr><th scope="col">Course Name</th><th scope="col">Grade</th><th scope="col">Enrolled On</th></tr></thead><tbody>' + dataCollector(data) + '</tbody></table>')
+  }
+
+  studentShowPageWinter(data) {
+    let emptyArray = []
+    function dataCollector(data) {
+    for (var i = 0; i < data.length; i++) {
+      let courseName = data[i]["course"]["name"]
+      let grade = data[i]["grade"]["letter"]
+      let enrolledOnRaw = data[i]["created_at"].split("T")[0]
+      // var enrolledOn = enrolledOnRaw.split("T")[1]
+      emptyArray.push('<tr><th scope="row">' + courseName + '</th><td>' + grade + '</td><td>' + enrolledOnRaw + '</td></tr>')
+    }
+    return emptyArray.join("")
+    }
+    $('div.winterinfo').append('<table class="table table-striped"<thead><tr><th scope="col">Course Name</th><th scope="col">Grade</th><th scope="col">Enrolled On</th></tr></thead><tbody>' + dataCollector(data) + '</tbody></table>')
+  }
+
 }
